16a89f174143f4e393cfdbc04f7bbda466c2d209
iambraj <138420969+iambraj@users.noreply.github.com>
2025-04-23 17:56:08 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ function Header() {
       <div id="header__bar" style = {{transform: showHeaderBar? `translateY(80px)`: `translateY(0px)`}}>
         <h3 className="header__bar__title">Github Code Base</h3>
         <div className="header__bar__icons">
-          <a href="https://github.com/AsHuToShSiNgH02/Sorting-Visualizer" target='_blank'><img src="https://img.icons8.com/ios/50/000000/github--v1.png"/></a>
+          <a href="https://github.com/iambraj/Sorting-Visualizer" target='_blank'><img src="https://img.icons8.com/ios/50/000000/github--v1.png"/></a>
         </div>
       </div>
 
